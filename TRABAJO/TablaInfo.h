a703f55b5496457f10585e36726676a43b4f01be
j.carnero <coque.carnero@gmail.com>
2025-06-22 20:22:40 -0700

    Me he dejado una clase por subir

--- TRABAJO/TablaInfo.h
+++ TRABAJO/TablaInfo.h
@@ -3,12 +3,16 @@
 
 struct TablaInfo
 {
+	//atributos
 	const int filas = 0, columnas = 0;
 	Color** data;
-	Color& operator () (int i, int j) { return data[i][j]; }
-	Color operator () (const Posicion& p)const { return data[p.fil][p.col]; }
-	TablaInfo (int f, int c) : filas{ f }, columnas{ c } {
+
+	//constructor
+	TablaInfo(int f, int c) : filas{ f }, columnas{ c } {
 		data[f][c];
 	}
-};
 
+	//operator
+	Color& operator () (int i, int j) { return data[i][j]; }
+	Color operator () (const Posicion& p)const { return data[p.fil][p.col]; }
+};
